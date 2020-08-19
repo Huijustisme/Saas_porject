02ffe11e5c2273390c200bafc40bf959489dbd34
clh <1170118659@qq.com>
2020-08-19 23:22:49 +0800

    SaaS_Export #项目第十一日代码
    1、独立完成出货表导出
    
    2、能够理解模板导出
    
    3、能够实现百万数据的Excel导出
    
    4、能够理解出口报运模块
    
    5、能够理解出口报运单的生成
    
    6、能够理解Webservice的定义

---
+++ export_cargo_service/src/main/java/com/itheima/service/cargo/impl/ExportServiceImpl.java
@@ -0,0 +1,102 @@
+package com.itheima.service.cargo.impl;
+
+import com.itheima.dao.cargo.*;
+import com.itheima.domain.cargo.*;
+import com.itheima.service.cargo.ExportService;
+import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.apache.shiro.crypto.hash.Hash;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.*;
+
+/**
+ * 报运单的业务实现
+ */
+@Service // 注意：必须是dubbo的注解
+public class ExportServiceImpl implements ExportService{
+    @Autowired
+    private ExportDao exportDao;//报运Dao
+    @Autowired
+    private ContractDao contractDao;//合同Dao
+    @Autowired
+    private ExportProductDao exportProductDao;
+
+    @Override
+    public PageInfo<Export> findByPage(ExportExample exportExample, int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum,pageSize);
+        return new PageInfo<>(exportDao.selectByExample(exportExample));
+    }
+
+    @Override
+    public List<Export> findAll(ExportExample exportExample) {
+        return exportDao.selectByExample(exportExample);
+    }
+
+    @Override
+    public Export findById(String id) {
+        return exportDao.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public void save(Export export) {
+        export.setId(UUID.randomUUID().toString());
+        //定义货物数量 及 附件数量
+        Integer proNum = 0;
+        Integer extNum = 0;
+        //1. 设置报运单的状态为0
+        export.setState(0);
+        //设置时间
+        export.setInputDate(new Date());
+        export.setCreateTime(new Date());
+        //切割合同ID
+        String contractIds = export.getContractIds();
+        String[] contractIdArray = contractIds.split(",");
+        //根据合同ID查询合同对象
+        String contractNoStr="";
+        for (String contractId : contractIdArray) {
+            Contract contract = contractDao.selectByPrimaryKey(contractId);
+            contractNoStr += contract.getContractNo()+"";
+            //计算报运单的货物数量 及 附件数量
+            proNum += contract.getProNum();
+            extNum += contract.getExtNum();
+        }
+        //去掉空格
+        contractNoStr = contractNoStr.trim();
+        //设置合同号
+        export.setCustomerContract(contractNoStr);
+        //设置货物和附件的数量
+        export.setProNum(proNum);
+        export.setExtNum(extNum);
+        //2. 往报运单表添加一条记录
+        exportDao.insertSelective(export);
+        //3. 修改报运单下的所有购销合同的状态（改为2）
+        //4. 把报运单下的所有购销合同的货物 拷贝到 报运商品表 下
+        //5. 把报运单下的所有购销合同的附件  拷贝到 报运商品附件表 下
+        exportDao.insertSelective(export);
+
+    }
+
+    @Override
+    public void update(Export export) {
+        //更新报运单
+        exportDao.updateByPrimaryKeySelective(export);
+
+        //更新报运商品数据
+        List<ExportProduct> exportProducts = export.getExportProducts();
+        if(exportProducts!=null && exportProducts.size()>0){
+
+            for(ExportProduct exportProduct:exportProducts){
+                exportProductDao.updateByPrimaryKeySelective(exportProduct);
+            }
+        }
+    }
+
+    @Override
+    public void delete(String id) {
+        exportDao.deleteByPrimaryKey(id);
+    }
+}
